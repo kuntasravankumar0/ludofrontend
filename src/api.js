d67596dba5e063dd832c26bec7b85c37a8a24714
sravan <kunta.sravan11111@gmail.com>
2026-06-05 07:50:40 -0700

    Initial commit

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "https://ludobackend-qszi.onrender.com";
+  import.meta.env.VITE_API_URL || "https://ludobackend-s71a.onrender.com";
 
 export const WS_BASE_URL =
-  import.meta.env.VITE_WS_URL || "wss://ludobackend-qszi.onrender.com";
+  import.meta.env.VITE_WS_URL || "wss://ludobackend-s71a.onrender.com";
 
 
 // export const API_BASE_URL =
